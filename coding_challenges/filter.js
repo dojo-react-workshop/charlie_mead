785cea0e0cec90b6bde25e0341b4b4ca6a156113
Charles Mead <meadch@gmail.com>
2017-05-22 10:35:29 -0500

    adds filter/map solutions

--- coding_challenges/filter.js
+++ coding_challenges/filter.js
@@ -1,1 +1,11 @@
-Array.prototype.filter = function() {}
+Array.prototype.filter = function(callback) {
+    const newArray = [];
+
+    for (let i = 0; i < this.length; i += 1){
+        if (callback(this[i])) {
+            newArray.push(this[i]);
+        }
+    }
+    
+    return newArray;
+}
--- coding_challenges/map.js
+++ coding_challenges/map.js
@@ -1,1 +1,13 @@
-Array.prototype.map = function() {}
+Array.prototype.map = function(callback){
+    const newArray = [];
+
+    for (let i = 0; i < this.length; i += 1){
+        newArray.push(callback(this[i]));
+    }
+    
+    return newArray;
+}
+
+
+
+
