fd72ba07f0aa82efca3dd605ee812618c50f9452
Charles Mead <meadch@gmail.com>
2017-05-24 13:01:30 -0500

    adds server code

--- node_and_apis/first_server/server.js
+++ node_and_apis/first_server/server.js
@@ -1,1 +1,11 @@
-console.log('server!');
+var http = require('http');
+var fs = require('fs');
+
+var server = http.createServer(function(request, response) {
+  console.log(request.url);
+  fs.readFile('./index.html', 'utf8', function(err, fileContents){
+    response.end(fileContents);
+  })
+});
+
+server.listen(8000);
