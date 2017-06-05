ea2df3a6964c432b78aa4162847f296c3c1dd566
Charles Mead <meadch@gmail.com>
2017-06-05 12:53:10 -0500

    2

--- diy_router/src/index.js
+++ diy_router/src/index.js
@@ -2,23 +2,22 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 
 const App = () => {
-    return <h1>App</h1>
+    return (
+        <div>
+            <Link path="/javascript">JavaScript</Link>
+            <Link path="/haskell">Haskell</Link>
+            <Link path="/coffeescript">CoffeeScript</Link>
+            <hr />
+        </div>
+    )
 }
 
-const Menu = () => {
-    return <h1>App</h1>
+const Link = (props) => {
+    return null;
 }
 
-const JavaScript = () => {
-    return <h1>JavaScript</h1>
-}
-
-const Haskell = () => {
-    return <h1>Haskell</h1>
-}
-
-const CoffeeScript = () => {
-    return <h1>CoffeeScript</h1>
-}
+const JavaScript = () => <p>A high-level, dynamic, untyped, and interpreted programming language.</p>
+const Haskell = () => <p>A standardized, general-purpose purely functional programming language, with non-strict semantics and strong static typing.</p>
+const CoffeeScript = () => <p>A programming language that transpiles into JavaScript, so we'll be redirecting there in 3.</p>
 
 ReactDOM.render(<App />, document.getElementById('root'));
