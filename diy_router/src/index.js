93f2bf11214d51776bbe466693fe7b721b69e2e0
Charles Mead <meadch@gmail.com>
2017-06-05 12:36:16 -0500

    initial

---
+++ diy_router/src/index.js
@@ -0,0 +1,24 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+const App = () => {
+    return <h1>App</h1>
+}
+
+const Menu = () => {
+    return <h1>App</h1>
+}
+
+const JavaScript = () => {
+    return <h1>JavaScript</h1>
+}
+
+const Haskell = () => {
+    return <h1>Haskell</h1>
+}
+
+const CoffeeScript = () => {
+    return <h1>CoffeeScript</h1>
+}
+
+ReactDOM.render(<App />, document.getElementById('root'));
