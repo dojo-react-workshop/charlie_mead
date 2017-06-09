fc60a1669881bd3a90747891e3d73e90ca07bfca
Charles Mead <meadch@gmail.com>
2017-06-09 09:23:20 -0500

    adds calendar solution

--- coding_challenges/mergedCalendar.js
+++ coding_challenges/mergedCalendar.js
@@ -55,3 +55,27 @@ const output = [
         endTime: 12
     }
 ]
+
+function mergeMeetings(meetingTimeRanges) {
+    meetingTimeRanges.sort((a, b) => a.startTime - b.startTime);
+
+    return meetingTimeRanges.reduce((mergedMeetings, currentMeeting) => {
+        if (mergedMeetings.length === 0) {
+            mergedMeetings.push(currentMeeting);
+            return mergedMeetings;
+        }
+
+        const lastMergedMeeting = mergedMeetings[mergedMeetings.length - 1];
+
+        if (lastMergedMeeting.endTime >= currentMeeting.startTime) {
+            lastMergedMeeting.endTime = Math.max(lastMergedMeeting.endTime, currentMeeting.endTime);
+            // lastMergedMeeting.endTime = (lastMergedMeeting.endTime > currentMeeting.endTime) ? lastMergedMeeting.endTime : currentMeeting.endTime;
+        } else {
+            mergedMeetings.push(currentMeeting);
+        }
+
+        return mergedMeetings;
+    }, []);
+}
+
+console.log(mergeMeetings(meetings));
